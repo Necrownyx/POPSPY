15c7376e754f2c7a684314186717c80e61c06ab7
Nyx <necrownyx@outlook.com>
2023-04-12 15:06:56 +1000

    creates save and load functions in data.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tools import *
 import poplib
 
 # string to import
+d = data.loadAccounts()
 
 email = account()
 email.importAccount("lboutestanfei198612@rambler.ru:4sH98svjLoK:pop.rambler.ru:True:26:Fri, 07 Apr 2023 07:44:28 +0300")
--- tools/data.py
+++ tools/data.py
@@ -2,42 +2,48 @@
 This file manages the data for the program. It will load and save the data this is to minimize the amount of time it takes to load the data into the program.
 The data is all stored in a single json file.
 '''
+def loadData(location='data.json'):
+    # check if the file exists
+    import os
 
-def loadAccounts(location='data.json'):
-    '''loads the data from the data.json file in the local directory unless the user has specified a different location.'''
-    # open the file and load the data
-    import json
-
-    try:
+    if os.path.exists(location):
+        # load the data
+        import json
         with open(location, 'r') as file:
             data = json.load(file)
-        # return the data
-        return data
-    except:
-        return {}
+        accounts = data['accounts']
+        settings = data['settings']
+        messages = data['messages']
 
-def saveAccounts(data={}, location='data.json'):
-    '''saves the data to the data.json file in the local directory unless the user has specified a different location.'''
-    # open the file and save the data
+        d = [accounts, settings, messages]
+        return d
+    else:
+        # create the file and rerun the function
+        saveData(None)
+        return loadData(location)
+    
+def saveData(data, location='data.json'):
     import json
 
+    # if the data is None then create a new data file
+    if data == None:
+        data = [{}, {}, {}]
+
+
+
+    # accoutns, settings, messages are all different dictionaries that need to be combined into one dictionary
+    accounts = data[0]
+    settings = data[1]
+    messages = data[2]
+
+    d = {'accounts': accounts, 'settings': settings, 'messages': messages}
+
     try:
+        # dump as indented json
         with open(location, 'w') as file:
-            json.dump(data, file)
+            json.dump(d, file, indent=4)
         return True
     except:
         return False
-
-def loadSettings():
-    pass
-
-def saveSettings():
-    pass
-
-def loadMessages():
-    pass
-
-def saveMessages():
-    pass
 
 
