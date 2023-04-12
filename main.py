8661da29b3fbcc4937e3dc0203f8fb0fee84d3a6
Nyx <necrownyx@outlook.com>
2023-04-12 15:07:54 +1000

    updated the rest of the project to reflect the change

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from tools import *
 import poplib
 
 # string to import
-d = data.loadAccounts()
+d = data.loadData()
+print(d)
 
 email = account()
 email.importAccount("lboutestanfei198612@rambler.ru:4sH98svjLoK:pop.rambler.ru:True:26:Fri, 07 Apr 2023 07:44:28 +0300")
